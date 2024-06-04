8fda06329b7f30916a779a728a62b4de1517d881
Pedro Vitor <pvitor20@hotmail.com>
2024-06-04 16:21:20 -0300

    first commit

---
+++ Desafio1.js
@@ -0,0 +1,34 @@
+let nome = "xXStarkXx";
+let xp = 100000;
+let mensagem = " está no nível de"
+
+if(xp <= 1000){
+    console.log(nome + mensagem + " Ferro" );
+}
+if (xp >= 1001 && xp<= 2000) {
+    console.log(nome + mensagem + " Bronze");
+    
+} 
+if (xp >= 2001 && xp<= 5000) {
+    console.log(nome + mensagem + " Prata");
+    
+}
+if (xp >= 5001 && xp<= 7000) {
+    console.log(nome + mensagem + " Ouro");
+    
+}
+if (xp >= 7001 && xp<= 8000) {
+    console.log(nome + mensagem + " Platina");
+    
+}
+if (xp >= 8001 && xp<= 9000) {
+    console.log(nome + mensagem + " Ascendente");
+    
+}
+if (xp >= 9001 && xp<= 10000) {
+    console.log(nome + mensagem + " Imortal");
+    
+}
+if (xp > 10000){
+    console.log(nome + mensagem + " Radiante")
+}
